d13699c9ba27cf0fa4d5d7dbbfbcf3cb8d5e7c13
fil <a21280323@isec.pt>
2023-12-26 18:37:28 +0000

    db inicial

--- src/main/java/isec/tp/das/onlinecompiler/OnlineCompilerApplication.java
+++ src/main/java/isec/tp/das/onlinecompiler/OnlineCompilerApplication.java
@@ -3,11 +3,14 @@ package isec.tp.das.onlinecompiler;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.sql.Connection;
+
 @SpringBootApplication
 public class OnlineCompilerApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(OnlineCompilerApplication.class, args);
+		Connection connection = Database.initiliazeDatabase();
 	}
 
 }
