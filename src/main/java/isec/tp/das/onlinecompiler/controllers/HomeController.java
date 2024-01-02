6237af5ba3b9530b6006ba52dd72abf3cd8b9954
fil <a21280323@isec.pt>
2024-01-02 19:59:12 +0000

    clean up pom file, remove thymeleaf

--- src/main/java/isec/tp/das/onlinecompiler/controllers/HomeController.java
+++ src/main/java/isec/tp/das/onlinecompiler/controllers/HomeController.java
@@ -1,13 +1,15 @@
 package isec.tp.das.onlinecompiler.controllers;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 
-@Controller
+@RestController
 public class HomeController {
     @GetMapping("/")
-    public String home() {
-        return "HomePage";
+    public ResponseEntity<String> home() {
+        return ResponseEntity.ok().build();
     }
 }
