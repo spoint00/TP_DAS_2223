c05f535948b016c45a4a18ce6c691400abc53264
RafaelFonseca <73261081+RafaelFonseca@users.noreply.github.com>
2024-01-17 22:43:00 +0000

    notifier funciona, já se recebe pings no discord

--- src/main/java/isec/tp/das/onlinecompiler/services/DiscordNotifier.java
+++ src/main/java/isec/tp/das/onlinecompiler/services/DiscordNotifier.java
@@ -4,49 +4,43 @@ import isec.tp.das.onlinecompiler.models.ProjectEntity;
 import isec.tp.das.onlinecompiler.models.ResultEntity;
 import org.springframework.stereotype.Service;
 
-import java.io.OutputStream;
-import java.net.HttpURLConnection;
-import java.net.URL;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
 
 @Service
-public class DiscordNotifier implements BuildListener{
+public class DiscordNotifier implements BuildListener {
     public DiscordNotifier() {
     }
+
+
     @Override
     public void onBuildCompleted(ProjectEntity project, ResultEntity result) {
-        String discordHook = "https://discord.com/api/webhooks/1197204619730886727/z4NZDbawY6BZ2ZV1qs4652dJjqnnwMd3MzKB4EPMb9rV3xSVG1oMoO9iXxXHU9iBYeLj";
 
-        // Construct the message
-        String message = String.format(
-                "Build Notification:\nProject Name: %s\nProject ID: %d\nResult: %s",
-                project.getName(),
-                project.getId(),
-                result.getMessage()
-        );
+        String discordHook = "https://discord.com/api/webhooks/1197204619730886727/z4NZDbawY6BZ2ZV1qs4652dJjqnnwMd3MzKB4EPMb9rV3xSVG1oMoO9iXxXHU9iBYeLj";
+        String projectName = project.getName();
+        long projectId = project.getId();
+
+        String jsonPayload = String.format("{\"content\": \"Build Notification\", \"embeds\": [{\"title\": \"Project Details\", \"fields\": [{\"name\": \"Project Name\", \"value\": \"%s\"}, {\"name\": \"Project ID\", \"value\": \"%s\"}]}]}", projectName, projectId);
+        System.out.println("Testing JsonPayLoad: " + jsonPayload);
+        HttpClient client = HttpClient.newHttpClient();
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(URI.create(discordHook))
+                .header("Content-Type", "application/json")
+                .POST(HttpRequest.BodyPublishers.ofString(jsonPayload))
+                .build();
 
         try {
-            URL url = new URL(discordHook);
-            String jsonPayload = "{\"content\":\"" + message + "\"}";
-            byte[] out = jsonPayload.getBytes();
-            HttpURLConnection http = (HttpURLConnection) url.openConnection();
-            http.setDoOutput(true);
-            http.setRequestMethod("POST");
-            http.addRequestProperty("Content-Type", "application/json");
-            http.setRequestProperty("Content-Length", String.valueOf(out.length));
-
-
-            OutputStream stream = http.getOutputStream();
-            stream.write(out);
-
-            System.out.println("Response Code: " + http.getResponseCode());
-            System.out.println("Response Message: " + http.getResponseMessage());
-
-            http.disconnect();
+            HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+            System.out.println("Response status code: " + response.statusCode());
+            System.out.println("Response body: " + response.body());
         } catch (Exception e) {
             e.printStackTrace();
         }
     }
 }
+
 
 
 
